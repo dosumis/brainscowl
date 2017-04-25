49e7d107139146274456ab6865a90be9c4d3e4d1
David Osumi-Sutherland <dosumis@gmail.com>
2017-04-25 18:28:16 +0100

    rearranging

--- src/dosumis/brainscowl/test/new_ont_query.scala
+++
@@ -1,7 +0,0 @@
-package org.dosumis.brainscowl.test
-import main.scala.org.dosumis.brainscowl.BrainScowl
-
-
- object test_load_ont extends(App) {
-//   }
-}
--- src/dosumis/brainscowl/test/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * 
- */
-/**
- * @author davidos
- *
- */
-package org.dosumis.brainscowl.test;
